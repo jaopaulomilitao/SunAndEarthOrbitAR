8dabf63be42b8d43e84a38f3b0dcd7bd5b5de2ec
João Paulo Militão <143536750+jaopaulomilitao@users.noreply.github.com>
2023-11-09 21:14:12 -0300

    First prototype
    Sun and earth orbit.

---
+++ script.js
@@ -0,0 +1,14 @@
+var terra = document.getElementById("terra");
+var sol = document.getElementById("sol");
+var terraEmOrbita = document.getElementById("paraAdicionar");
+var terraSozinha = document.getElementById("paraRemover");
+
+terra.addEventListener('markerFound', function () {
+    if (terra.object3D.visible && sol.object3D.visible) {
+        terraEmOrbita.setAttribute('visible', true);
+        terraSozinha.setAttribute('visible', false);
+    } else {
+        terraEmOrbita.setAttribute('visible', false);
+        terraSozinha.setAttribute('visible', true);
+    }
+});
